54cb7dbb9a55b3786b3e3063d50b4bf7c2c0e3d2
Caike <caikecolantonio@hotmail.com>
2020-09-18 14:28:08 -0300

    Deploy

---
+++ license/admin.py
@@ -0,0 +1,13 @@
+from django.contrib import admin
+from license.models import License
+from license.models import Client
+
+
+
+
+# Register your models here.
+
+admin.site.register(License)
+admin.site.register(Client)
+
+
---
+++ license/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class LicenseConfig(AppConfig):
+    name = 'license'
+
